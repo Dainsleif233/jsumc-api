a1565321c9e009dfdc182f62da9f07becfc60160
Dainsleif <dainsleif@yeah.net>
2025-12-13 12:21:25 +0800

    11

--- edge-functions/sjmcl/rss/[[src]].ts
+++ edge-functions/sjmcl/rss/[[src]].ts
@@ -1,14 +1,16 @@
 import transform from '../../../src/sjmcl/rss';
 import Logger from '../../../src/utils/Logger';
 
-export function onRequestGet(ctx: any): Promise<Response>{
+export async function onRequestGet(ctx: any): Promise<Response>{
     const params: string[] = ctx.params.src;
+    
+    return new Response(JSON.stringify(params));
 
-    const src = params.join('/');
-    const srcUrl = new URL(src);
-    const originalUrl = new URL(ctx.request.url);
+    // const src = params.join('/');
+    // const srcUrl = new URL(src);
+    // const originalUrl = new URL(ctx.request.url);
 
-    const log = new Logger('SJMCL/RSS');
-    log.info('Fetching {}', src);
-    return transform(srcUrl, originalUrl);
+    // const log = new Logger('SJMCL/RSS');
+    // log.info('Fetching {}', src);
+    // return transform(srcUrl, originalUrl);
 }
